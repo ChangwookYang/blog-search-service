552eb7798b3d13ef45e70dac014fc93016a6d5ca
changwook yang <ycw92325@gmail.com>
2023-03-19 17:35:02 +0900

    #9 - Entity 생성

---
+++ common/src/main/java/com/blog/search/common/domain/BlogSearchResults.java
@@ -0,0 +1,33 @@
+package com.blog.search.common.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlogSearchResults {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String keyword;
+    private Long searchCount;
+
+    @CreatedDate
+    private LocalDateTime createdDt;
+    @LastModifiedDate
+    private LocalDateTime updatedDt;
+}
