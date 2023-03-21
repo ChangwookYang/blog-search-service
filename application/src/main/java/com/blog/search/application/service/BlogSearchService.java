473b9a3c22d0e6b2d67fae9f13de72b99e44e94d
changwook yang <ycw92325@gmail.com>
2023-03-21 22:07:02 +0900

    #16 - 테스트 환경 세팅

--- application/src/main/java/com/blog/search/application/service/BlogSearchService.java
+++ application/src/main/java/com/blog/search/application/service/BlogSearchService.java
@@ -7,11 +7,13 @@ import com.blog.search.common.service.BlogSearchApiServiceFactory;
 import com.blog.search.common.type.ApiType;
 import com.blog.search.common.type.SortType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BlogSearchService {
@@ -19,6 +21,7 @@ public class BlogSearchService {
     @Retryable(value = {Exception.class}, maxAttempts = 1)
     @Transactional(readOnly = true)
     public SearchListResponse searchBlogs(String keyword, SortType sortType, int pageNumber, int pageSize) throws Exception {
+        log.info("search blogs - keyword : {}, apiType : {}", keyword, ApiType.KAKAO);
 
         BlogSearchApiInterface<?> blogSearchApiService = BlogSearchApiServiceFactory.getService(ApiType.KAKAO);
 
@@ -28,6 +31,7 @@ public class BlogSearchService {
     @Recover
     @Transactional(readOnly = true)
     public SearchListResponse searchBlogs(Exception e, String keyword, SortType sortType, int pageNumber, int pageSize) throws Exception {
+        log.info("search blogs recover - keyword : {}, apiType : {}", keyword, ApiType.NAVER);
 
         BlogSearchApiInterface<?> blogSearchApiService = BlogSearchApiServiceFactory.getService(ApiType.NAVER);
 
--- application/src/main/resources/data.sql
+++ application/src/main/resources/data.sql
@@ -4,3 +4,13 @@ INSERT INTO blog_search_results (keyword, search_result, sort_type, api_type, cr
 
 INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('안녕', 3, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
 INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('하이', 7, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('안녕1', 13, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('하이1', 27, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('안녕2', 33, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('하이2', 57, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('안녕3', 63, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('하이3', 77, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('안녕4', 9, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('하이4', 1, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('안녕5', 2, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
+INSERT INTO blog_search_keywords (keyword, search_count, created_dt, updated_dt) VALUES ('하이5', 10, '2020-01-03 11:00:00', '2020-01-03 11:00:00');
