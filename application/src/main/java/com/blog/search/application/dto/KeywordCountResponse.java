8d910bf4567794f5feb968fa78cd7af1497d5d7c
changwook yang <ycw92325@gmail.com>
2023-03-22 21:51:46 +0900

    #22-swagger ui 2.9.2 적용
    http://localhost:8080/swagger-ui.html

--- application/src/main/java/com/blog/search/application/dto/KeywordCountResponse.java
+++ application/src/main/java/com/blog/search/application/dto/KeywordCountResponse.java
@@ -1,15 +1,20 @@
 package com.blog.search.application.dto;
 
 import com.blog.search.common.domain.BlogSearchKeywords;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "인기 키워드 결과", description = "인기 키워드 응답 Class")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class KeywordCountResponse {
+    @ApiModelProperty(example = "맛집", value = "키워드")
     String keyword;
+    @ApiModelProperty(example = "10", value = "키워드 검색 수")
     Long searchCount;
 
     public static KeywordCountResponse from(BlogSearchKeywords entity) {
--- common/src/main/java/com/blog/search/common/dto/SearchApiResponse.java
+++ common/src/main/java/com/blog/search/common/dto/SearchApiResponse.java
@@ -3,6 +3,8 @@ package com.blog.search.common.dto;
 import com.blog.search.common.dto.kakao.DocumentDto;
 import com.blog.search.common.dto.naver.ItemDto;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,16 +12,24 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+@ApiModel(value = "블로그 글 응답", description = "블로그 글 응답 Class")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class SearchApiResponse {
+
+    @ApiModelProperty(value = "블로그 글 제목", example = "블로그 글 제목")
     private String title;
+    @ApiModelProperty(value = "블로그 글 내용", example = "블로그 글 내용")
     private String contents;
+    @ApiModelProperty(value = "블로그 글 URL", example = "블로그 글 URL")
     private String url;
+    @ApiModelProperty(value = "블로그 이름", example = "블로그 이름")
     private String blogName;
+    @ApiModelProperty(value = "블로그 썸네일", example = "Image URL")
     private String thumbnail;
+    @ApiModelProperty(value = "블로그 생성 일자", example = "2033-03-22")
     private LocalDate createdDate;
 
     public static SearchApiResponse of(String title, String contents, String url, String blogName, String thumbnail, LocalDate createdDate) {
@@ -37,4 +47,5 @@ public class SearchApiResponse {
         return SearchApiResponse.of(itemDto.getTitle(), itemDto.getDescription(), itemDto.getLink(), itemDto.getBloggername()
                 , null, LocalDate.parse(itemDto.getPostdate(), formatter));
     }
+
 }
